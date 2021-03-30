2ea8c272900d06111e5045e7604260e2b69fab4c
reader-wh94 <sstt9884@gmail.com>
2021-03-30 18:36:07 +0900

    Study tabBarController and MapKit

---
+++ ch06-1891321-tabBarController/ch06-1891321-tabBarController/CityViewContorller.swift
@@ -0,0 +1,61 @@
+//
+//  ViewController.swift
+//  ch06-1891321-tabBarController
+//
+//  Created by mac027 on 2021/03/30.
+//
+
+import UIKit
+
+class CityViewController: UIViewController {
+    
+    @IBOutlet weak var cityPickerView: UIPickerView!
+    var cities: [String: [String:Double]] = [
+        "Seoul":["lon":126.9778, "lat":37.5683], "Athens":["lon":23.7162, "lat":37.9795],
+        "Bangkok":["lon":100.5167, "lat":13.75], "Berlin":["lon":13.4105, "lat":52.5244],
+        "Jerusalem":["lon":35.2163, "lat":31.769], "Lisbon":["lon":-9.1333, "lat":38.7167],
+        "London":["lon":-0.1257, "lat":51.5085], "New York":["lon":-74.006, "lat":40.7143],
+        "Paris":["lon":2.3488, "lat":48.8534], "Sydney":["lon":151.2073, "lat":-33.8679],
+    ]
+}
+
+extension CityViewController{
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        print("CityViewController.viewDidLoad")
+        
+        cityPickerView.dataSource = self
+        cityPickerView.delegate = self
+    }
+}
+
+extension CityViewController: UIPickerViewDataSource {
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return Array(cities.keys).count
+    }
+}
+
+extension CityViewController: UIPickerViewDelegate {
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) ->
+    String? {
+        let sorted = Array(cities.keys).sorted()
+        return sorted[row]
+    }
+}
+
+extension CityViewController {
+    func getCurrentLonLat() -> (String, Double?, Double?) {
+        var cityNames = Array(cities.keys)
+        cityNames.sort()
+        let selectedIndex = cityPickerView.selectedRow(inComponent: 0)
+        let selectedCity = cityNames[selectedIndex]
+        let city = cities[selectedCity]
+        return (selectedCity, city!["lon"], city!["lat"])
+    }
+}
---
+++ ch06-1891321-tabBarController/ch06-1891321-tabBarController/MapViewController.swift
@@ -0,0 +1,65 @@
+//
+//  MapViewController.swift
+//  ch06-1891321-tabBarController
+//
+//  Created by mac027 on 2021/03/30.
+//
+
+import UIKit
+import MapKit
+
+class MapViewController: UIViewController {
+
+    @IBOutlet weak var mapView: MKMapView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        print("MapViewController.viewDidLoad")
+    }
+
+}
+
+extension MapViewController{
+    @IBAction func segmentedControlValueChanged(_ sender: UISegmentedControl) {
+        switch sender.selectedSegmentIndex {
+        case 0:
+            mapView.mapType = .standard
+        case 1:
+            mapView.mapType = .hybrid
+        case 2:
+            mapView.mapType = .satellite
+        default:
+            break
+        }
+    }
+}
+
+extension MapViewController{
+    override func viewWillAppear(_ animated: Bool) {
+        print("MapViewController.viewWillAppear")
+        
+        let tabBarController = parent as! UITabBarController
+        let cityViewController = tabBarController.viewControllers![0] as! CityViewController
+        let (city, longitute, latitute) = cityViewController.getCurrentLonLat()
+        
+        updateMap(title: city, longitute: longitute, lattitute: latitute)
+    }
+}
+
+extension MapViewController{
+    func  updateMap(title: String, longitute: Double?, lattitute: Double?) {
+        let span = MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1)
+        var center = mapView.centerCoordinate
+        if let lon = longitute, let lat = lattitute{
+            center = CLLocationCoordinate2D(latitude: lat, longitude: lon)
+        }
+        let region = MKCoordinateRegion(center: center, span: span)
+        mapView.setRegion(region, animated: true)
+        
+        let annotation = MKPointAnnotation()
+        annotation.coordinate = center
+        annotation.title = title
+        mapView.addAnnotation(annotation)
+    }
+}
